276afff61fb6e64bd82f36cd9854f0a87ce54617
vaishnvi shetty <142256509+cherokee22@users.noreply.github.com>
2024-09-22 13:11:16 +0530

    Update reversetriangle.py

--- reversetriangle.py
+++ reversetriangle.py
@@ -18,3 +18,16 @@ for i in range(11):
 
     print()
 
+''' printing proper middle tiangle ''' 
+n = 4
+for i in range(1, n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(" ", end="")
+
+        # Print asterisks for the current row
+        for k in range(1, 2*i):
+            print("*", end="")
+        print()
+
+
