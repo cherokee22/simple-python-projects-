4167d064374307ef30b6edd40aa4ae0c7165013e
vaishnvi shetty <142256509+cherokee22@users.noreply.github.com>
2024-09-22 12:57:20 +0530

    Update reversetriangle.py

--- reversetriangle.py
+++ reversetriangle.py
@@ -8,6 +8,14 @@ for i in range(11):
     i=i-1
     for j in range(i,10):
         print("*",end="")
+        print()
+
+''' for print the proper left triangle '''
+for i in range(11):
+    i=i-1
+    for j in range(i):
+        print("*",end="")
 
 
     print()
+
